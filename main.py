26815f896fc56ecaa265f6651ea92adbf4a5a8f9
Inuk <inuk.baik@gmail.com>
2024-03-16 01:00:16 -0400

    ♻️ Move data to separate JSON file
    Morse Code dictionary is now in data.json file

--- main.py
+++ main.py
@@ -1,82 +1,31 @@
-morse_code_dict = {
-    '.-': 'A',
-    '-...': 'B',
-    '-.-.': 'C',
-    '-..': 'D',
-    '.': 'E',
-    '..-.': 'F',
-    '--.': 'G',
-    '....': 'H',
-    '..': 'I',
-    '.---': 'J',
-    '-.-': 'K',
-    '.-..': 'L',
-    '--': 'M',
-    '-.': 'N',
-    '---': 'O',
-    '.--.': 'P',
-    '--.-': 'Q',
-    '.-.': 'R',
-    '...': 'S',
-    '-': 'T',
-    '..-': 'U',
-    '...-': 'V',
-    '.--': 'W',
-    '-..-': 'X',
-    '-.--': 'Y',
-    '--..': 'Z',
-    '-----': '0',
-    '.----': '1',
-    '..---': '2',
-    '...--': '3',
-    '....-': '4',
-    '.....': '5',
-    '-....': '6',
-    '--...': '7',
-    '---..': '8',
-    '----.': '9',
-    '.-.-.-': '.',
-    '--..--': ',',
-    '..--..': '?',
-    '.----.': "'",
-    '-.-.--': '!',
-    '-..-.': '/',
-    '-.--.': '(',
-    '-.--.-': ')',
-    '.-...': '&',
-    '---...': ':',
-    '-.-.-.': ';',
-    '-...-': '=',
-    '.-.-.': '+',
-    '-....-': '-',
-    '..--.-': '_',
-    '.-..-.': '"',
-    '...-..-': '$',
-    '.--.-.': '@',
-    '...---...': 'SOS'
-}
+import json
+
 
 def morse_to_english(morse_code):
-    morse_code = morse_code.split(' ')
-    english = ''
-    for code in morse_code:
-        if code in morse_code_dict:
-            english += morse_code_dict[code]
-        else:
-            english += ' '
+    with open('data.json', 'r') as file:
+        morse_code_dict = json.load(file)
+        morse_code = morse_code.split(' ')
+        english = ''
+        for code in morse_code:
+            if code in morse_code:
+                english += morse_code_dict[code]
+            else:
+                english += ' '
     return english
 
 
 def english_to_morse(english):
-    english = english.upper()
-    morse_code = ''
-    for letter in english:
-        if letter == ' ':
-            morse_code += ' '
-        else:
-            for key, value in morse_code_dict.items():
-                if value == letter:
-                    morse_code += key + ' '
+    with open('data.json', 'r') as file:
+        morse_code_dict = json.load(file)
+        english = english.upper()
+        morse_code = ''
+        for letter in english:
+            if letter == ' ':
+                morse_code += ' '
+            else:
+                for key, value in morse_code_dict.items():
+                    if value == letter:
+                        morse_code += key + ' '
     return morse_code
 
 
