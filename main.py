b50661e14ea7ef22cb32bfde2a13d0d4f525032a
Inuk <inuk.baik@gmail.com>
2024-03-16 00:35:33 -0400

    🎉 Add basic function

---
+++ main.py
@@ -0,0 +1,61 @@
+morse_code_dict = {
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '.': 'E',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z'
+}
+
+def morse_to_english(morse_code):
+    morse_code = morse_code.split(' ')
+    english = ''
+    for code in morse_code:
+        if code in morse_code_dict:
+            english += morse_code_dict[code]
+        else:
+            english += ' '
+    return english
+
+
+def english_to_morse(english):
+    english = english.upper()
+    morse_code = ''
+    for letter in english:
+        if letter == ' ':
+            morse_code += ' '
+        else:
+            for key, value in morse_code_dict.items():
+                if value == letter:
+                    morse_code += key + ' '
+    return morse_code
+
+
+select = input('Enter 1 for Morse to English, Enter 2 for English to Morse: ')
+
+if select == '1':
+    morse_code = input('Enter Morse Code: ')
+    print(morse_to_english(morse_code))
+elif select == '2':
+    english = input('Enter English: ')
+    print(english_to_morse(english))
